6a3d5b388a783055e1eb70a876913bc93ee5ec37
Kyuunex <kyuunex@gmail.com>
2020-01-09 19:52:35 +0400

    bump version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='aioosuapi',
     packages=['aioosuapi'],
-    version='r20191124',
+    version='r20200109',
     description='Asynchronous osu! api wrapper',
     author='Kyuunex',
     url='https://github.com/Kyuunex/aioosuapi',
-    download_url='https://github.com/Kyuunex/aioosuapi/tarball/r20191124',
+    download_url='https://github.com/Kyuunex/aioosuapi/tarball/r20200109',
     keywords=['osu', 'api'],
     classifiers=[],
     requires=['aiohttp'],
