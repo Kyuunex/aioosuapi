6be447c2a5858d21493d00ef08c3694a5b20dc9d
Kyuunex <kyuunex@protonmail.ch>
2020-04-15 16:02:43 +0400

    Semantic versioning

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='aioosuapi',
     packages=['aioosuapi'],
-    version='r20200109',
+    version='1.0.0',
     description='Asynchronous osu! api wrapper',
     author='Kyuunex',
     url='https://github.com/Kyuunex/aioosuapi',
-    download_url='https://github.com/Kyuunex/aioosuapi/tarball/r20200109',
+    download_url='https://github.com/Kyuunex/aioosuapi/tarball/1.0.0',
     keywords=['osu', 'api'],
     classifiers=[],
     requires=['aiohttp'],
