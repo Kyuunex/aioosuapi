953e92ac5a2bcad33738b31a70319161da18615f
Kyuunex <kyuunex@protonmail.ch>
2020-08-01 04:35:33 +0400

    bump version, a very barebones, placeholder release

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='aioosuwebapi',
     packages=['aioosuwebapi'],
-    version='2.0.0-placeholder',
+    version='2.0.0-placeholder.1',
     description='Asynchronous osu! api wrapper',
     author='Kyuunex',
     url='https://github.com/Kyuunex/aioosuapi',
-    download_url='https://github.com/Kyuunex/aioosuapi/tarball/2.0.0-placeholder',
+    download_url='https://github.com/Kyuunex/aioosuapi/tarball/2.0.0-placeholder.1',
     keywords=['osu', 'api'],
     classifiers=[],
     requires=['aiohttp'],
