3218381d16435792e878ab402bdc01789c5f4cd2
Kyuunex <kyuunex@protonmail.ch>
2021-04-21 12:50:38 +0400

    fix requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='aioosuapi',
     packages=['aioosuapi'],
-    version='1.2.0',
+    version='1.2.1',
     description='Asynchronous osu! api wrapper',
     author='Kyuunex',
     url='https://github.com/Kyuunex/aioosuapi',
-    download_url='https://github.com/Kyuunex/aioosuapi/tarball/1.2.0',
+    download_url='https://github.com/Kyuunex/aioosuapi/tarball/1.2.1',
     keywords=['osu', 'api'],
     classifiers=[],
-    requires=['aiohttp'],
+    install_requires=['aiohttp'],
 )
