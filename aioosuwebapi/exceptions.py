490220094d9ae0b6933a9039c88042c4ae291d07
Kyuunex <kyuunex@protonmail.ch>
2022-10-24 01:03:05 +0400

    add exceptions.py

---
+++ aioosuwebapi/exceptions.py
@@ -0,0 +1,16 @@
+class AuthenticationError(Exception):
+    """ Authentication Error. """
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class HTTPException(Exception):
+    """ Connection Error. """
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class OtherOsuAPIError(Exception):
+    """ Error. """
+    def __init__(self, *args, **kwargs):
+        pass
