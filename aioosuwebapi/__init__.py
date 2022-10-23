1b26881d1eaf852a60e0375cfc3ff7ddf71026e0
Kyuunex <kyuunex@protonmail.ch>
2022-10-24 01:08:52 +0400

    move scraper class to it's own file

--- aioosuwebapi/__init__.py
+++ aioosuwebapi/__init__.py
@@ -4,8 +4,6 @@ An asynchronous osu! API wrapper
 
 import aiohttp
 import asyncio
-import json
-from bs4 import BeautifulSoup
 
 from aioosuwebapi.exceptions import AuthenticationError
 from aioosuwebapi.exceptions import HTTPException
@@ -144,59 +142,3 @@ class aioosuwebapi:
             if 'error' in response_contents:
                 return []
             return response_contents
-
-
-class aioosuwebscraper:
-    def __init__(self):
-        self._base_url = "https://osu.ppy.sh/"
-        self._scrape_session = aiohttp.ClientSession()
-
-    async def close(self):
-        await self._scrape_session.close()
-
-    async def scrape_beatmapset_discussions_array(self, beatmapset_id):
-        async with self._scrape_session.get(self._base_url + f"beatmapsets/{beatmapset_id}/discussion") as response:
-            response_contents = await response.text()
-            if len(response_contents) < 5:
-                raise HTTPException("Connection issues")
-
-        if "json-beatmapset-discussion" in response_contents:
-            soup = BeautifulSoup(response_contents, "html.parser")
-            results = soup.find(id="json-beatmapset-discussion").string.strip()
-            return json.loads(results)
-        elif "<h1>Page Missing</h1>" in response_contents:
-            # TODO: this should also raise a ValueError
-            # what im doing is very retarded
-            return {
-                "beatmapset": {
-                    "status": "deleted"
-                }
-            }
-        else:
-            raise OtherOsuAPIError("Endpoint has most likely been changed")
-
-    async def scrape_latest_ranked_beatmapsets_array(self):
-        async with self._scrape_session.get(self._base_url + "beatmapsets") as response:
-            response_contents = await response.text()
-            if len(response_contents) < 5:
-                raise HTTPException("Connection issues")
-
-        if not "json-beatmaps" in response_contents:
-            raise OtherOsuAPIError("Endpoint has most likely been changed")
-
-        soup = BeautifulSoup(response_contents, "html.parser")
-        results = soup.find(id="json-beatmaps").string.strip()
-        return json.loads(results)
-
-    async def scrape_group_members_array(self, group_id):
-        async with self._scrape_session.get(self._base_url + f"groups/{group_id}") as response:
-            response_contents = await response.text()
-            if len(response_contents) < 5:
-                raise HTTPException("Connection issues")
-
-        if not "json-users" in response_contents:
-            raise OtherOsuAPIError("Endpoint has most likely been changed")
-
-        soup = BeautifulSoup(response_contents, "html.parser")
-        result = soup.find(id="json-users").string.strip()
-        return json.loads(result)
