ec9c239f87a8a1758f632877e197bbcb9133e2ad
Kyuunex <kyuunex@protonmail.ch>
2022-11-27 22:32:25 +0400

    Revert "remove discussions from scaper"
    This reverts commit 7bc92ae2709a1f0a8feb0af0a228a9127d09e6d9.

--- aioosuwebapi/scraper.py
+++ aioosuwebapi/scraper.py
@@ -14,6 +14,21 @@ class aioosuwebscraper:
     async def close(self):
         await self._scrape_session.close()
 
+    async def scrape_beatmapset_discussions_array(self, beatmapset_id):
+        async with self._scrape_session.get(self._base_url + f"beatmapsets/{beatmapset_id}/discussion") as response:
+            response_contents = await response.text()
+            if len(response_contents) < 5:
+                raise HTTPException("Connection issues")
+
+        if "json-beatmapset-discussion" in response_contents:
+            soup = BeautifulSoup(response_contents, "html.parser")
+            results = soup.find(id="json-beatmapset-discussion").string.strip()
+            return json.loads(results)
+        elif "<h1>Page Missing</h1>" in response_contents:
+            return {}
+        else:
+            raise OtherOsuAPIError("Endpoint has most likely been changed")
+
     async def scrape_latest_ranked_beatmapsets_array(self):
         async with self._scrape_session.get(self._base_url + "beatmapsets") as response:
             response_contents = await response.text()
